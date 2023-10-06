b1672b2a01b0aee775b5832e3c88463f6fff4239
el-dAna <atadanasl@gmail.com>
2023-10-06 18:09:22 +0300

    Completed the checks for the 5volts from Arduiuno to DAC and DAC vout to electrodes. If any is false a warning appears on screen suggesting which is broken. If corrected Active stimulations starts as expected

--- school_projects/tubitak_arduiuno_code/tubitak_arduiuno_code.ino
+++ school_projects/tubitak_arduiuno_code/tubitak_arduiuno_code.ino
@@ -13,20 +13,26 @@ const int electrode = A0; //9V red cable voltage from regulator t arduiuno
 const int five_volt_probe = A2; //5V orange cable from arduiuno to DAC Vin
 const int DAC_output_voltage_probe = A1; // blue cable to measure output voltage of DAC
 
-float  Set_DAC_Vout = 1460; // Tune DAC Vout here
+// Due to circuit connections, the usual output range of DAC(0-5V) changed to (0-3.6)V. The old integer input to DAC was (0-4095) from Datasheet
+// The DAC object now takes integer values from (0-3000) to output (0-3.6)V.
+float  Set_DAC_Vout = 1480; // Tune DAC Vout here to ouput the required voltage for required current(10mA total) bu inputing values in range (0-3000)
+float expected_DAC_Vout = (183/152000)*Set_DAC_Vout;
+float low_expected_DAC_Vout = ((183/152000)*Set_DAC_Vout)-0.1;
+float high_expected_DAC_Vout = ((183/152000)*Set_DAC_Vout)+0.1;
 
+
+// LCD Variables. Check out this blog post https://toptechboy.com/arduino-tutorial-48-connecting-and-using-an-lcd-display/
 int rs=7;
 int en=8;
 int d4=9;
 int d5=6;
 int d6=11; // 11, 5
 int d7=12;
-LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
+LiquidCrystal lcd(rs,en,d4,d5,d6,d7); 
 
 
 bool all_passed;
 int number_of_warns = 1;
-
 String Welcome = "tDCS stimulation starts in...";
 String stimulationMessage = "Active hdTDCs....";
 String afterStimulation = "Stimulation Done!";
@@ -35,32 +41,32 @@ String afterStimulation = "Stimulation Done!";
 
 void setup(){
 
-  
-
+  //Initial count-down to the start of stimulation
   Serial.begin(9600);
-  hdTDCs_DAC.begin(DAC_output_pin);     //initialize
+  hdTDCs_DAC.begin(DAC_output_pin);
   lcd.begin(numCols, numRows);
   OnStart(Welcome, scrollSpeed_beforeStimulation, 0.1);
   lcd.clear();
   delay(2000);
 
-  // run_checks_again: // a checkpoint to return to if any of the checks fail. if any check fail then 10mA current will not be output!
-  // //all_passed = Run_Checks(Set_DAC_Vout);
 
-  // if (!all_passed){
-  //   number_of_warns++;
-  //   goto run_checks_again;
-  // }
+
+  // Checking for voltages. Precisely the 5v input to DAC(Blue) and Vout(Orange) from DAC to electrodes
   
-
+  run_checks_again: // a checkpoint to return to if any of the checks fail. if any check fail then 10mA current will not be output!
+  all_passed = Run_Checks(low_expected_DAC_Vout, high_expected_DAC_Vout);
+  if (!all_passed){
+    number_of_warns++;
+    goto run_checks_again;
+  };
 }
 
 
 void loop() {  
 
-  // hdTDCs_DAC.analogWrite(1660);
-  Stimulation(true, Set_DAC_Vout);
-  OnStart(stimulationMessage, scrollSpeed_forStimulation, 0.2);
+  // Start of stimulation if all tests passed, thus we have the correct voltages at the check points
+  Stimulation(true, Set_DAC_Vout); //
+  OnStart(stimulationMessage, scrollSpeed_forStimulation, 0.2); //to display message during stimulation
   lcd.clear();
   lcd.setCursor(0, 0);
 
@@ -85,8 +91,10 @@ void Stimulation(bool active, float set_DAC_Vout){
 
 
 
+
+
 bool assertWithErrorMessage(bool condition, String possible_error, float measured_voltage) {
-  bool passed_check;
+  bool passed_check = true;
   if (!condition) {
     //If monitor available
     Serial.print("WARNING ");
@@ -133,31 +141,35 @@ float get_voltage(int AnaloguePin, bool SerialMonitor) {
 }
 
 
-// bool Run_Checks(float DAC_Vout){
-//   int checks_list[3];
 
-//   //Checks 9V from voltage regulator to arduiuno
-//   float voltage_at_electrodes = get_voltage(electrode, false);
-//   bool condition_A0 = voltage_at_electrodes >= 9; //this is fixed, Vout of 9V regulator
-//   bool condition_A0_result = assertWithErrorMessage(condition_A0, "Wrong eltrd. V!", voltage_at_electrodes);
+
+bool Run_Checks(float lower_limit_DAC_Voltage_Out, float upper_limit_DAC_Voltage_Out ){
+
+  bool checks_list[3]; // initialised list to store outcome of checks for checks_list[0],9V, checks_list[1],5V to DAC and checks_list[2],Vout from DAC
+
+  // 9V is too big to connect to Analogue pin and have another external power source. This breaks down the arduiuno
+  // //Checks 9V from voltage regulator to arduiuno
+  // float voltage_at_electrodes = get_voltage(electrode, false);
+  // bool condition_A0 = voltage_at_electrodes >= 9; //this is fixed, Vout of 9V regulator
+  // bool condition_A0_result = assertWithErrorMessage(condition_A0, "Wrong eltrd. V!", voltage_at_electrodes);
   
-//   //Checks 5V from arduiuno to DAC
-//   float 5V_to_DAC_voltage = get_voltage(five_volt_probe, false);
-//   bool condition_A2 = 5V_to_DAC_voltage >= 4.7; // this is fixed, close to 5V from arduiuno
-//   bool condition_A2_result = assertWithErrorMessage(condition_A2, "Wrong DAC V_in!", 5V_to_DAC_voltage);
+  //Checks 5V from arduiuno to DAC
+  float Five_Volt_to_DAC = get_voltage(five_volt_probe, false);
+  bool condition_A2 = Five_Volt_to_DAC >= 4.7; // this is fixed, close to 5V from arduiuno
+  bool condition_A2_result = assertWithErrorMessage(condition_A2, "Wrong DAC V_in!", Five_Volt_to_DAC);
 
-//   //checks the Vout of DAC to electrodes
-//   float DAC_Vout = get_voltage(DAC_output_voltage_probe, false);
-//   bool condition_A1 = DAC_Vout >= DAC_Vout; //tune DAC to output 10mA and set that voltage
-//   bool condition_A1_result = assertWithErrorMessage(condition_A1, "Wrong DAC V_out!", DAC_Vout);
+  //checks the Vout of DAC to electrodes
+  float DAC_Vout = get_voltage(DAC_output_voltage_probe, false);
+  bool condition_A1 = (DAC_Vout >= lower_limit_DAC_Voltage_Out) & (DAC_Vout <= upper_limit_DAC_Voltage_Out); //tune DAC to output 10mA and set that voltage 
+  bool condition_A1_result = assertWithErrorMessage(condition_A1, "Wrong DAC V_out!", DAC_Vout);
 
-//   checks_list[0] = condition_A0_result;
-//   checks_list[1] = condition_A1_result;
-//   checks_list[2] = condition_A2_result;
-//   bool all_checks_passed = checks_list[0] & checks_list[1] & checks_list[2];
+  //checks_list[0] = condition_A0_result;
+  checks_list[1] = condition_A1_result;
+  checks_list[2] = condition_A2_result;
+  bool all_checks_passed = checks_list[1] & checks_list[2]; // & checks_list[0]; 
 
-//   return all_checks_passed;
-// }
+  return all_checks_passed;
+}
 
 
 
